7f3e3736be7b418d554440f82b16f18b00c43ccc
taras <nazarko94@gmail.com>
2023-01-23 14:19:15 +0200

    got dates from github

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { AlertContext } from '../context/alert/alertContext'
+import { GithubContext } from '../context/github/githubContext';
 
 const Search = () => {
+  const [value, setValue] = useState('');
   const {show} = useContext(AlertContext);
+  const github = useContext(GithubContext);
 
   const onSubmit = event => {
-    if(event.key === 'Enter') {
-      show('This is alert!')
+    if(event.key !== 'Enter') {
+      return
+    }
+    if(value.trim()) {
+      github.search(value.trim());
+    } else {
+      show('ENTER USER DATES!');
     }
   }
 
@@ -17,6 +25,8 @@ const Search = () => {
         className="form-control"
         placeholder="Enter username..."
         onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
       />
     </div>
   )
