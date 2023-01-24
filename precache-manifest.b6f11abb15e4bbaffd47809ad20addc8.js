bb71fbf7eede47e61f064438398732bee5950689
taras <nazarko94@gmail.com>
2023-01-24 19:13:40 +0200

    Updates

---
+++ precache-manifest.b6f11abb15e4bbaffd47809ad20addc8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f95ce23ec1d35c81d9a1c51f80826c",
+    "url": "/react-github-search/index.html"
+  },
+  {
+    "revision": "befe8b52967c4c9e1511",
+    "url": "/react-github-search/static/css/main.05bb447c.chunk.css"
+  },
+  {
+    "revision": "fda6b3e1f654cac40ce3",
+    "url": "/react-github-search/static/js/2.36c2a1c7.chunk.js"
+  },
+  {
+    "revision": "befe8b52967c4c9e1511",
+    "url": "/react-github-search/static/js/main.e7ddd7a1.chunk.js"
+  },
+  {
+    "revision": "5b31de643e0f738515dc",
+    "url": "/react-github-search/static/js/runtime~main.3eb55b5e.js"
+  }
+]);
